0ebe9f617e6f85058c1a6c14cea1d42a8f1a6618
dantona02 <danielmiksch3001@gmail.com>
2023-10-29 17:32:23 +0100

    Added class HaloPoint

---
+++ halo_point.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[53]:
+
+
+import numpy as np
+
+
+# In[54]:
+
+
+class HaloPoint:
+    def __init__(self, ax, mass, color_decay, color1, color2='white'):
+        self.halo_points_color1 = [ax.plot([], [], 'o', c=color1, markersize=0, alpha=0, markeredgecolor='none', zorder=3)[0] for _ in range(100)]
+        self.halo_points_white = [ax.plot([], [], 'o', c=color2, markersize=0, alpha=0, markeredgecolor='none', zorder=4)[0] for _ in range(100)]
+        self.mass = mass
+        self.color_decay = color_decay
+        
+    def set_data(self, x, y):
+        radius = np.linspace(2, 20, 100)
+        for index, i in enumerate(radius):
+            self.halo_points_color1[index].set_data([x], [y])
+            self.halo_points_white[index].set_data([x], [y])
+            
+            self.halo_points_color1[index].set_markersize(self.mass*i)
+            self.halo_points_color1[index].set_alpha(np.exp(-(i-2))**self.color_decay)
+            
+            self.halo_points_white[index].set_markersize(self.mass*i)
+            self.halo_points_white[index].set_alpha(np.exp(-(i-2))**1)
+        
+    def get_artists(self):
+        return self.halo_points_color1 + self.halo_points_white
+
