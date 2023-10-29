4a3435df5a850f678f947287cb687d287b25e045
dantona02 <danielmiksch3001@gmail.com>
2023-10-29 22:20:29 +0100

    Added trajectory

--- halo_point.py
+++ halo_point.py
@@ -24,3 +24,42 @@ class HaloPoint:
         
     def get_artists(self):
         return self.halo_points_color1 + self.halo_points_white
+
+
+class Trajectory:
+    def __init__(self, ax, size, datarange, color_decay, color, edgecolor):
+        self.trajectory_main = [ax.plot([], [], c=color, linewidth=1, alpha=0, zorder=4)[0] for _ in
+                                range(datarange - 1, 0, -1)]
+        self.trajectory_side = [ax.plot([], [], c=edgecolor, linewidth=2, alpha=0, zorder=3)[0] for _ in
+                                range(datarange - 1, 0, -1)]
+        self.size = size
+        self.color_decay = color_decay
+        self.datarange = datarange
+
+    def set_data(self, x, y, frame):
+        if len(x) != len(y):
+            raise ValueError("x and y must have the same number of elements")
+        # for traj in self.trajectory_main + self.trajectory_side:
+        #     traj.set_data([], [])
+        start_index = max(0, frame - self.datarange)
+        subset_x = x[start_index:frame + 1]
+        subset_y = y[start_index:frame + 1]
+
+        num_segments = min(len(self.trajectory_main), len(subset_x) - 1)  # segments available
+
+        for s in range(num_segments):
+            segment_start = s  # start point of segment
+            segment_end = s + 1  # end point of segment
+            traj_index = num_segments - 1 - s  # position in list of trajectories in reversed order
+            fraction = 1 - (s / num_segments)
+            alpha = np.exp(-self.color_decay * fraction) * (1 - fraction)
+
+            self.trajectory_main[traj_index].set_data(subset_x[segment_start:segment_end + 1],
+                                                      subset_y[segment_start:segment_end + 1])
+            self.trajectory_side[traj_index].set_data(subset_x[segment_start:segment_end + 1],
+                                                      subset_y[segment_start:segment_end + 1])
+            self.trajectory_main[traj_index].set_alpha(alpha)
+            self.trajectory_side[traj_index].set_alpha(alpha * 0.2)
+
+    def get_artists(self):
+        return self.trajectory_main + self.trajectory_side
